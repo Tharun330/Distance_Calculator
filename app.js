3aadda70b9489ddeeac066702fc29ff92f3280b8
Tharun_Gudla <tharunteja.gudla@gmail.com>
2025-03-25 14:49:31 -0500

    GET, POST api created & implemented basic middleware for logging

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const History = require('./models/history')
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const MONGO_URL = 'mongodb://localhost:27017/distancecalculator';
-
 
 main()
     .then(() => {
@@ -18,11 +19,36 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.use(cors({
+    origin: 'fsefsfefs',
+    credentials: true
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
 
 
-app.get('/test', (req, res) => {
+app.get('/history', async (req, res) => {
 
-    res.send('Hi root');
+    let data = await History.find({})
+    console.log(`Data from DB: ${data ? 'data retrieved from DB successful' : 'Error in retriev data from db'}`)
+    res.send(data);
+
+})
+
+app.post('/history/new', async (req, res) => {
+
+    let newHistory = new History(req.body);
+    console.log(`Data Received from client-side: ${newHistory}`);
+    await newHistory.save();
+    res.json({
+        message: 'Form Data received'
+    })
 
 })
 
